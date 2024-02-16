d6f8963c29837ac55eb3bab1fdce6549b9c34334
Ruslan Voshchylo <ruslan.voshchylo2@gmail.com>
2024-02-16 21:58:17 +0200

    add almost final footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Outlet } from "react-router-dom";
+import { Footer } from "./components/Footer/Footer";
 import "./App.css";
 
-const App = () => <Outlet />;
+const App = () => <Footer />;
 
 export default App;
