7baf5a0f545a2fa35317fc93338d92552290adf9
Dmytro Yarechuk <retrodn14@gmail.com>
2024-02-20 21:58:38 +0200

    created about section with adaptive layout

--- src/components/UI/AboutSection/AboutSection.tsx
+++ src/components/UI/AboutSection/AboutSection.tsx
@@ -1,84 +1,69 @@
 import React from "react";
-import { Phone } from "../../../types/Phone";
+import { Item } from "../../../types/Item";
 
 type Props = {
-  phone: Phone;
+  item: Item;
 };
 
-export const AboutSection: React.FC<Props> = ({ phone }) => {
+export const AboutSection: React.FC<Props> = ({ item }) => {
   const {
-    description,
-    screen,
-    resolution,
-    processor,
-    ram,
+    id,
+    namespaceId,
+    name,
+    capacityAvailable,
     capacity,
-    camera,
-    zoom,
-    cell,
-  } = phone;
+    priceRegular,
+    priceDiscount,
+    colorsAvailable,
+    color,
+    images,
+    description,
+    ...techSpecs
+  } = item;
 
   return (
-    <div className="flex h-full flex-col text-[20px]/[25px] font-semibold">
-      <div className="mb-14 flex flex-col gap-y-8">
-        <h2 className="border-b pb-4 font-extrabold sm:text-[22px]">About</h2>
-        {description.map((section) => (
-          <div key={section.title} className="">
-            <h3 className="mb-4 text-base/[21px] font-bold sm:text-[20px]/[25px]">
-              {section.title}
-            </h3>
-            <p className="text-sm/[21px] font-medium text-secondary">
-              {section.text.map((paragraph) => (
-                <>
-                  <div>{paragraph}</div>
-                  {paragraph !== section.text[section.text.length - 1] && (
-                    <br />
-                  )}
-                </>
-              ))}
-            </p>
+    <div>
+      <div className="grid grid-cols-4 gap-x-4 font-semibold sm:mb-10 sm:grid-cols-12 xl:grid-cols-24">
+        <div className="col-span-full mb-14 grid gap-y-8 xl:col-span-12">
+          <h2 className="border-b pb-4 text-[20px]/[26px] font-bold sm:text-[22px]/[31px] sm:font-extrabold">
+            About
+          </h2>
+          {description.map((section) => (
+            <div key={section.title}>
+              <h3 className="mb-4 text-[16px]/[21px] font-bold sm:text-[20px]/[26px]">
+                {section.title}
+              </h3>
+              <p className="text-[14px]/[21px] font-semibold text-secondary xl:font-medium">
+                {section.text.map((paragraph) => (
+                  <>
+                    <div>{paragraph}</div>
+                    {paragraph !== section.text[section.text.length - 1] && (
+                      <br />
+                    )}
+                  </>
+                ))}
+              </p>
+            </div>
+          ))}
+        </div>
+        <div className="col-span-full flex flex-col gap-y-8 xl:col-start-[14] xl:col-end-[-1]">
+          <h2 className="border-b pb-4 text-[20px]/[26px] font-bold sm:text-[22px]/[31px] sm:font-extrabold">
+            Tech specs
+          </h2>
+          <div className="grid gap-y-2">
+            {Object.entries(techSpecs).map(([key, value]) => (
+              <div className="flex justify-between">
+                <span className="text-[14px]/[21px] font-medium text-secondary">
+                  {key === "ram"
+                    ? `${key.toUpperCase()}`
+                    : `${key.charAt(0).toUpperCase()}${key.slice(1)}`}
+                </span>
+                <span className="max-w-48 text-right text-[14px]/[21px] text-primary sm:max-w-full">
+                  {Array.isArray(value) ? value.join(", ") : value}
+                </span>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="flex flex-col gap-y-2">
-        <h2 className="mb-[30px] border-b pb-4 font-bold sm:mb-[25px] sm:font-extrabold">
-          Tech specs
-        </h2>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Screen</span>
-          <span className="text-sm text-primary">{screen}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Resolution</span>
-          <span className="text-sm text-primary">{resolution}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Processor</span>
-          <span className="text-sm text-primary">{processor}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">RAM</span>
-          <span className="text-sm text-primary">{ram}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">
-            Built in memory
-          </span>
-          <span className="text-sm text-primary">{capacity}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Camera</span>
-          <span className="text-sm text-primary">{camera}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Zoom</span>
-          <span className="text-sm text-primary">{zoom}</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-sm font-medium text-secondary">Cell</span>
-          <span className="max-w-[50%] text-right text-sm text-primary">
-            {cell.join(", ")}
-          </span>
         </div>
       </div>
     </div>
