1c376f14762975433e804596a9bb34e459d9dbdf
Dmytro Yarechuk <retrodn14@gmail.com>
2024-02-29 00:43:57 +0200

    fixes

--- src/Pages/Contacts.tsx
+++ src/Pages/Contacts.tsx
@@ -10,10 +10,10 @@ export const Contacts = () => {
     <>
       <div className="container flex min-h-[calc(100vh-64px-98px)] flex-col items-center">
         <h1 className="mb-8 pt-8 text-center text-[48px]/[56px] font-bold text-primary dark:text-dark-white">
-          Our team
+          Team PAKA
         </h1>
         <div className="flex w-full flex-col flex-wrap items-start justify-between gap-y-8 pb-8 sm:flex-row sm:justify-evenly xl:justify-evenly">
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 href="https://github.com/dyarechuk"
@@ -29,7 +29,7 @@ export const Contacts = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-y-1">
               <h3 className="text-center text-[22px]/[27px] font-bold text-primary dark:text-dark-white">
-                Dmytro Yarechuk
+                Dmytro (darkmode skeleton) Yarechuk
               </h3>
               <a
                 rel="noreferrer"
@@ -44,7 +44,7 @@ export const Contacts = () => {
               </a>
             </div>
           </div>
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 rel="noreferrer"
@@ -60,7 +60,7 @@ export const Contacts = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-y-1">
               <h3 className="text-center text-[22px]/[27px] font-bold text-primary dark:text-dark-white">
-                Ruslan Voschylo
+                Ruslan (URKLAN(Pivo)) Voschylo
               </h3>
               <a
                 rel="noreferrer"
@@ -75,7 +75,7 @@ export const Contacts = () => {
               </a>
             </div>
           </div>
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 href="https://github.com/AntonDuchenko"
@@ -83,7 +83,7 @@ export const Contacts = () => {
                 target="_blank"
               >
                 <img
-                  className="block h-[120px] w-[120px] rounded-lg object-contain transition-all hover:scale-[1.1] dark:rounded-none xl:max-h-[160px] xl:max-w-[160px]"
+                  className="block w-[120px] rounded-lg object-contain transition-all hover:scale-[1.1] dark:rounded-none xl:h-[120px] xl:max-h-[160px] xl:max-w-[160px]"
                   src={a_duchenko}
                   alt="Anton"
                 />
@@ -91,7 +91,7 @@ export const Contacts = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-y-1">
               <h3 className="text-center text-[22px]/[27px] font-bold text-primary dark:text-dark-white">
-                Anton Duchenko
+                Anton (ch1llov@rny@) Duchenko
               </h3>
               <a
                 href="https://github.com/AntonDuchenko"
@@ -106,7 +106,7 @@ export const Contacts = () => {
               </a>
             </div>
           </div>
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 href="https://github.com/yaneznayou"
@@ -122,7 +122,8 @@ export const Contacts = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-y-1">
               <h3 className="text-center text-[22px]/[27px] font-bold text-primary dark:text-dark-white">
-                Nikita Prokopenko
+                Nikita <br />
+                (JSON perevozchik) Prokopenko
               </h3>
               <a
                 href="https://github.com/yaneznayou"
@@ -137,7 +138,7 @@ export const Contacts = () => {
               </a>
             </div>
           </div>
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 href="https://github.com/rtkachuk-moon"
@@ -153,7 +154,7 @@ export const Contacts = () => {
             </div>
             <div className="flex flex-col items-center justify-center gap-y-1">
               <h3 className="text-center text-[22px]/[27px] font-bold text-primary dark:text-dark-white">
-                Rostyslav Tkachuk
+                Rostyslav (Rest in Pepsi) Tkachuk
               </h3>
               <a
                 href="https://github.com/rtkachuk-moon"
