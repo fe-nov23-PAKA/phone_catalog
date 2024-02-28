90990e3bcbd0a6f1099fea0cb9a670f9c16d9297
Anton Duchenko <deragandi0@gmail.com>
2024-02-28 21:54:30 +0200

    fix poblems after merging

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -30,6 +30,7 @@ export const Cart = () => {
     }, 800);
 
     AOS.init();
+
     return () => clearTimeout(timerId);
   }, []);
 
@@ -44,53 +45,63 @@ export const Cart = () => {
         >
           Cart
         </h1>
-        <div className="grid gap-x-4 md:grid-cols-3">
-          <div className="mb-8 flex flex-col gap-y-4 md:col-span-2">
-            {cartItems.map((item) =>
-              isLoading ? (
-                <CartItemSkeleton />
-              ) : (
-                <CartItem item={item} key={item.product.id} />
-              ),
+        {cartItems.length === 0 ? (
+          <div className="flex flex-col items-center">
+            <img
+              className="ml-auto mr-auto"
+              src={imgEmptyCart}
+              alt="Empty cart"
+            />
+          </div>
+        ) : (
+          <div className="grid gap-x-4 md:grid-cols-3">
+            <div className="mb-8 flex flex-col gap-y-4 md:col-span-2">
+              {cartItems.map((item) =>
+                isLoading ? (
+                  <CartItemSkeleton />
+                ) : (
+                  <CartItem item={item} key={item.product.id} />
+                ),
+              )}
+            </div>
+            {isLoading ? (
+              <CartCheckoutSkeleton />
+            ) : (
+              <div
+                data-aos="fade-left"
+                className="flex max-h-[190px] flex-col items-center justify-center rounded-[16px] border border-element-color p-6 dark:rounded-none dark:border-dark-elements"
+              >
+                <div className="flex w-full flex-col justify-center gap-y-4 text-center">
+                  <div className="border-b border-element-color pb-4 dark:border-dark-elements">
+                    <h2 className="text-[32px]/[41px] font-extrabold tracking-[-0.01em] dark:text-dark-white">
+                      ${totalPrice}
+                    </h2>
+                    <div className="text-center text-sm/[21px] font-semibold text-secondary">
+                      {`Total for ${allItemsCount === 1 ? `${allItemsCount} item` : `${allItemsCount} items`}`}
+                    </div>
+                  </div>
+                  <button
+                    className={classNames(
+                      "min-h-12 w-full rounded-lg bg-accent text-[14px]/[21px] font-semibold text-white transition-all hover:shadow-sh1 dark:rounded-none dark:bg-dark-accent dark:text-dark-white dark:hover:bg-dark-hover dark:hover:shadow-zinc-700",
+                      { "bg-icons-color hover:shadow-none": !cartItems.length },
+                    )}
+                    type="button"
+                    disabled={!cartItems.length}
+                    onClick={() => {
+                      setIsModalOpen(true);
+                      confetti({
+                        particleCount: 100,
+                        spread: 70,
+                      });
+                    }}
+                  >
+                    Checkout
+                  </button>
+                </div>
+              </div>
             )}
           </div>
-          {isLoading ? (
-            <CartCheckoutSkeleton />
-          ) : (
-            <div
-              data-aos="fade-left"
-              className="flex max-h-[190px] flex-col items-center justify-center rounded-[16px] border border-element-color p-6 dark:rounded-none dark:border-dark-elements"
-            >
-              <div className="flex w-full flex-col justify-center gap-y-4 text-center">
-                <div className="border-b border-element-color pb-4 dark:border-dark-elements">
-                  <h2 className="text-[32px]/[41px] font-extrabold tracking-[-0.01em] dark:text-dark-white">
-                    ${totalPrice}
-                  </h2>
-                  <div className="text-center text-sm/[21px] font-semibold text-secondary">
-                    {`Total for ${allItemsCount === 1 ? `${allItemsCount} item` : `${allItemsCount} items`}`}
-                  </div>
-                </div>
-                <button
-                  className={classNames(
-                    "min-h-12 w-full rounded-lg bg-accent text-[14px]/[21px] font-semibold text-white transition-all hover:shadow-sh1 dark:rounded-none dark:bg-dark-accent dark:text-dark-white dark:hover:bg-dark-hover dark:hover:shadow-zinc-700",
-                    { "bg-icons-color hover:shadow-none": !cartItems.length },
-                  )}
-                  type="button"
-                  disabled={!cartItems.length}
-                  onClick={() => {
-                    setIsModalOpen(true);
-                    confetti({
-                      particleCount: 100,
-                      spread: 70,
-                    });
-                  }}
-                >
-                  Checkout
-                </button>
-              </div>
-            </div>
-          )}
-        </div>
+        )}
       </div>
       {isModalOpen && <CheckoutModal />}
     </>
