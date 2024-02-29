dfc3bf68751282b8fe69570c312f6bdcb2cfcfaa
Dmytro Yarechuk <retrodn14@gmail.com>
2024-02-29 02:00:01 +0200

    add fixes

--- src/Pages/Contacts.tsx
+++ src/Pages/Contacts.tsx
@@ -3,6 +3,7 @@ import r_voshchylo from "../assets/img/team_photos/r_voshchylo.jpg";
 import d_iarechuk from "../assets/img/team_photos/d_iarechuk.jpg";
 import n_prokopenko from "../assets/img/team_photos/n_prokopenko.png";
 import r_tkachuk from "../assets/img/team_photos/r_tkachuk.jpg";
+import seventh from "../assets/img/team_photos/1_7.jpg";
 import { Github } from "../icons/Github";
 
 export const Contacts = () => {
@@ -22,7 +23,7 @@ export const Contacts = () => {
               >
                 <img
                   className="block h-[120px] w-[120px] rounded-lg object-contain transition-all hover:scale-[1.1] dark:rounded-none xl:max-h-[160px] xl:max-w-[160px]"
-                  src={d_iarechuk}
+                  src={seventh}
                   alt="Dmytro"
                 />
               </a>
@@ -138,7 +139,7 @@ export const Contacts = () => {
               </a>
             </div>
           </div>
-          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
+          <div className="flex h-full w-full flex-col items-center justify-start gap-y-4 rounded-lg border border-element-color bg-white p-6 shadow-lg shadow-zinc-400 backdrop-grayscale-0 dark:rounded-none dark:border-dark-surface2 dark:bg-dark-surface2 dark:shadow-zinc-700 sm:h-[328px] sm:w-[30%] xl:w-2/12 xl:shadow-2xl xl:shadow-zinc-600 xl:dark:shadow-zinc-700">
             <div>
               <a
                 href="https://github.com/rtkachuk-moon"
@@ -146,7 +147,7 @@ export const Contacts = () => {
                 target="_blank"
               >
                 <img
-                  className="block h-[120px] w-[120px] rounded-lg object-contain transition-all hover:scale-[1.1] dark:rounded-none xl:max-h-[160px] xl:max-w-[160px]"
+                  className="block h-[120px] w-[120px] rounded-lg object-contain backdrop-grayscale transition-all hover:scale-[1.1] dark:rounded-none xl:max-h-[160px] xl:max-w-[160px]"
                   src={r_tkachuk}
                   alt="Rostyslav"
                 />
